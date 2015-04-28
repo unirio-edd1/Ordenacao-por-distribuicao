098adfec12b9d1e9e9b307798f1c1cc8dbd8e001
diogomartins <diogo.martins@unirio.br>
2015-04-28 10:03:43 -0300

    Initial Commit

---
+++ Trabalho 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Trabalho 2
+//
+//  Created by Diogo Martins on 4/28/15.
+//  Copyright (c) 2015 Diogo Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
