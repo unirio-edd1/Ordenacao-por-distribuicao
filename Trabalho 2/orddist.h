771014008a08749fe43e5d1f8e939b7b6f51d09c
diogomartins <diogo.martins@unirio.br>
2015-05-06 14:43:21 -0300

    Refatoração

--- Trabalho 2/orddist.h
+++ Trabalho 2/orddist.h
@@ -3,9 +3,10 @@
 
 class orddist{
 protected:
-    int b; /* base */
-    int d; /* comprimento m ́aximo do digito */ lista* filas; /* vetor de listas */
-    lista L; /* lista inicial e que sera ordenada */
+    int b;              /* base */
+    int d;              /* comprimento m ́aximo do digito */
+    lista* filas;       /* vetor de listas */
+    lista L;            /* lista inicial e que sera ordenada */
 public:
     orddist(int base = 10);
     void carrega();
