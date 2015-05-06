5fe44559242046c9076bd059658041edeffeeb2f
diogomartins <diogo.martins@unirio.br>
2015-05-06 17:24:22 -0300

    Implementação da primeira parte do método orddist::carrega(), sem contemplar o calculo de maior_comprimento

--- Trabalho 2/orddist.cpp
+++ Trabalho 2/orddist.cpp
@@ -7,6 +7,20 @@
 //
 
 #include "orddist.h"
+#define TOTAL_DE_ITEMS 5
+
+int Menu()
+{
+    int escolha;
+    
+    cout << "--------------------------\n";
+    cout << "Selecione uma opcao:\n";
+    cout << "1. Insere elemento (no. inteiro) na lista\n";
+    cout << "2. Fim.\n";
+    cout << "Sua opcao: ";
+    cin >> escolha;
+    return escolha;
+}
 
 /*
  a. O construtor da classe atualiza o valor de b = 10 e inicializa o vetor filas com b elementos do tipo lista.
@@ -18,6 +32,21 @@ orddist::orddist(int base): b(base), filas(new lista[30])
  b. Função void carrega(). Esta função lê, via teclado, uma lista de números e insere em L. Em seguida, calcula o maior comprimento da representação das chaves na base b e armazena seu valor no atributo d da classe. Para tal, chama a função maior_comprimento da classe lista.
  */
 void orddist::carrega(){
+    int item, escolha;
+    bool fim = false;
+    
+    while((!fim) && (escolha = Menu())){
+        switch(escolha) {
+            case 1:
+                cout << "Entre com elemento para inserir: ";
+                cin >> item;
+                L.insere(item);
+                break;
+            case 2:
+                fim = true;
+                break;
+        }
+    }
     
 }
 
