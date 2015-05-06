aa3bdc2ec705f391c3eb1f57b4c2da3520460a1b
diogomartins <diogo.martins@unirio.br>
2015-05-06 12:25:08 -0300

    Adicionada implementação de lista::imprime

--- Trabalho 2/lista.cpp
+++ Trabalho 2/lista.cpp
@@ -3,3 +3,12 @@
 bool lista::vazia(){
     return (prim == NULL);
 }
+
+/* f. Função imprime(), que imprime os elementos da lista. */
+void lista::imprime() const{
+    elo *p;
+    cout << "Elementos da lista" << endl;
+    for(p=prim; p!=NULL; p=p->prox)
+        cout << p->dado << ' ';
+    cout << endl;
+}
